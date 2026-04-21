aab0e75b442110057e1bea8e92964baee05467bb
Igor da Silva Dias <rogigor1712@gmail.com>
2026-04-21 13:00:27 -0300

    apaguei tudo de desnecessário

--- app/inicio.jsx
+++ app/inicio.jsx
@@ -1,35 +1,11 @@
 "use client";
 
 import NavBar from "./navbar";
-import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase";
-import { useRouter } from "next/navigation";
 
-export default function Home() {
-    const [user, setUser] = useState(null);
-    const router = useRouter();
-
-    useEffect(() => {
-        async function checkUser() {
-            const { data } = await supabase.auth.getUser();
-
-            if (!data.user) {
-                router.push("/auth/login");
-            } else {
-                setUser(data.user);
-            }
-        }
-
-        checkUser();
-    }, []);
-
-    if (!user) return <p>Carregando...</p>;
-
-    return (
+export default function Home(){
+    return(
         <div>
-            <NavBar />
             <h1>Vexly</h1>
-            <p>Logado como: {user.email}</p>
         </div>
-    );
+    )
 }
