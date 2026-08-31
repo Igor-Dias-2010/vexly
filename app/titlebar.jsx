635ff3803741b8f81356a79343c8265d08faba9d
Igor da Silva Dias <rogigor1712@gmail.com>
2026-08-30 22:39:48 -0300

    Atualiza nome da imagem da titlebar

--- app/titlebar.jsx
+++ app/titlebar.jsx
@@ -5,7 +5,7 @@ export default function TitleBar() {
         <div className="titlebar">
             <nav>
                 <figure>
-                    <Image src={'/vexly-logo-oficial-removebg-preview.png'} alt="Logo do Vexly" width={270} height={170} loading="eager" draggable="false" />
+                    <Image src={'/vexly-logo.png'} alt="Logo do Vexly" width={270} height={170} loading="eager" draggable="false" />
                 </figure>
             </nav>
         </div>
