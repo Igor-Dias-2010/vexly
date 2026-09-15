ba222d3dfe6e997223f8a0081a10f30ce65dd685
Igor da Silva Dias <rogigor1712@gmail.com>
2026-09-15 11:30:38 +0000

    estiliza as tlas de autentucação

--- app/components/login/page.jsx
+++ app/components/login/page.jsx
@@ -2,17 +2,23 @@ import Link from "next/link";
 
 export default function Login() {
     return (
-        <div className="login-signup">
-            <h1>Entrar na sua conta</h1>
-            <label htmlFor="email">Digite seu Email</label>
-            <input type="text" placeholder="Digite seu Email" id="email" />
-            <label htmlFor="senha">Digite sua senha</label>
-            <input type="password" placeholder="Digite sua senha" id="senha" />
-            <button>Entrar</button>
-            <p>
-                Não tem uma conta?
-                <Link href="/components/signup">Crie uma agora</Link>
-            </p>
+        <div className="auth-container">
+            <div className="auth">
+                <h1>Entrar na sua conta</h1>
+                <div className="text-field">
+                    <label htmlFor="email">Digite seu Email</label>
+                    <input type="text" placeholder="Digite seu Email" id="email" />
+                </div>
+                <div className="text-field">
+                    <label htmlFor="senha">Digite sua senha</label>
+                    <input type="password" placeholder="Digite sua senha" id="senha" />
+                </div>
+                <button>Entrar</button>
+                <p>
+                    Não tem uma conta?
+                    <Link href="/components/signup">Crie uma agora</Link>
+                </p>
+            </div>
         </div>
     );
 }
--- app/components/signup/page.jsx
+++ app/components/signup/page.jsx
@@ -2,17 +2,23 @@ import Link from "next/link";
 
 export default function Signup() {
     return (
-        <div className="login-signup">
-            <h1>Criar conta</h1>
-            <label htmlFor="email">Digite um Email</label>
-            <input type="text" placeholder="Digite um Email" id="email" />
-            <label htmlFor="senha">Digite uma senha</label>
-            <input type="password" placeholder="Digite uma senha" id="senha" />
-            <button>Criar conta</button>
-            <p>
-                Já tem uma conta?
-                <Link href="/components/login">Entre na sua conta</Link>
-            </p>
+        <div className="auth-container">
+            <div className="auth">
+                <h1>Criar conta</h1>
+                <div className="text-field">
+                    <label htmlFor="email">Digite um Email</label>
+                    <input type="text" placeholder="Digite um Email" id="email" />
+                </div>
+                <div className="text-field">
+                    <label htmlFor="senha">Digite uma senha</label>
+                    <input type="password" placeholder="Digite uma senha" id="senha" />
+                </div>
+                <button>Criar conta</button>
+                <p>
+                    Já tem uma conta?
+                    <Link href="/components/login">Entre na sua conta</Link>
+                </p>
+            </div>
         </div>
     );
 }
